ef3dc461f23b9acfe3a333b4b2b3f4e08158a5b9
venkat-subrahmaniyan <vsb2903@gmail.com>
2016-04-02 14:34:04 +0530

    another

---
+++ +-.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter a number\n");
+scanf("%d",&a);
+if(a>0)
+{
+printf("The number is positive\n");
+}
+else if(a<0)
+{
+printf("The number is negative");
+}
+else
+{
+printf("The number is zero");
+}
+}
