1694c17c28f89e8b7f1bcdac09b06c39963763e2
venkat-subrahmaniyan <vsb2903@gmail.com>
2016-06-19 18:06:28 +0530

    28

---
+++ q.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+	char c;
+	printf("Enter a character\n");
+	scanf("%c",&c);
+	if(c=='q')
+	{
+		return 0;
+	}
+	else
+	{
+		printf("\enter q to exit");
+	}
+	
+}
